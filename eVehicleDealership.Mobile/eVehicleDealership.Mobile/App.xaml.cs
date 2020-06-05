96139d50b95979c97025c70949170409e66eeb58
Emin Hajdarević <emin.hajdarevic@edu.fit.ba>
2020-06-05 12:33:37 +0200

    New SyncFusion license

--- eVehicleDealership.Mobile/eVehicleDealership.Mobile/App.xaml.cs
+++ eVehicleDealership.Mobile/eVehicleDealership.Mobile/App.xaml.cs
@@ -10,7 +10,7 @@ namespace eVehicleDealership.Mobile
     {
         public App()
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjUwOTUxQDMxMzgyZTMxMmUzMEtOeml5K3g1SitTQ0hNdWdyVWRZS1hOcmRnQWM1b1dzWVQ0OVNxUTl4cTQ9");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjY3ODI5QDMxMzgyZTMxMmUzME5sQS8yVjhWN2RzOUNpdm9RWFhOM3pmcFpWcDNKZC9wRnpvZDZOTFFhL0k9");
             InitializeComponent();
             //var accessToken = Preferences.Get("access_token", string.Empty);
             //if (string.IsNullOrEmpty(accessToken))
